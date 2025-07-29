bb5a281ad63be5d30439c371aa8b8f7f9f45644d
vgsurendar <35067833+vgsurendar@users.noreply.github.com>
2025-07-29 14:34:02 +0530

    Time: 8 ms (18.59%), Space: 48.8 MB (14.23%) - LeetHub

---
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.c
@@ -0,0 +1,38 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+bool isPalindrome(struct ListNode* head) {
+    int size = 0, i = 0;
+    struct ListNode* temp = head;
+
+    while (temp != NULL) {
+        size++ ;
+        temp = temp->next;
+    }
+
+    int *arr = (int *)malloc(size * sizeof(int));
+
+    temp = head;
+
+    for (i = 0; i < size; i++) {
+        arr[i] = temp->val;
+        temp = temp->next;
+    }
+
+    temp = head;
+    i = (size - 1);
+
+    while (temp != NULL) {
+        if (temp->val != arr[i]) {
+            return false;
+        }
+        temp = temp->next;
+        i-- ;
+    }
+    
+    return true;
+}
